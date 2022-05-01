08fe669c350561a3fc5ff157cf162ab602df4a56
Rayehe <rayehetabatabaei1986@gmail.com>
2022-05-01 11:34:42 +0430

    Project Initialization

---
+++ src/Shop.Specs/Infrastructure/EFDataContextDatabaseFixture.cs
@@ -0,0 +1,22 @@
+﻿using Shop.Persistence.EF;
+using Shop.Specs.Infrastructure;
+using Xunit;
+
+namespace Shop.Specs.Infrastructure
+{
+    [Collection(nameof(ConfigurationFixture))]
+    public class EFDataContextDatabaseFixture : DatabaseFixture
+    {
+        readonly ConfigurationFixture _configuration;
+
+        public EFDataContextDatabaseFixture(ConfigurationFixture configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public EFDataContext CreateDataContext()
+        {
+            return new EFDataContext(_configuration.Value.DbConnectionString);
+        }
+    }
+}
