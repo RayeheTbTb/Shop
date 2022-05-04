cc941b0235a43907abf377c140e35370ce87a546
Rayehe <rayehetabatabaei1986@gmail.com>
2022-05-04 18:40:49 +0430

    Added CategoriesController

--- src/Shop.RestAPI/Controllers/CategoriesController.cs
+++ src/Shop.RestAPI/Controllers/CategoriesController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Services.Categories.Contracts;
+using Shop.Services.Products.Contracts;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Shop.RestAPI.Controllers
 {
@@ -6,6 +10,41 @@ namespace Shop.RestAPI.Controllers
     [Route("api/categories")]
     public class CategoriesController : Controller
     {
-        
+        private readonly CategoryService _service;
+
+        public CategoriesController(CategoryService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public void Add(AddCategoryDto dto)
+        {
+            _service.Add(dto);
+        }
+
+        [HttpPut("{id}")]
+        public void Update(int id, UpdateCategoryDto dto)
+        {
+            _service.Update(id, dto);
+        }
+
+        [HttpGet]
+        public IList<GetCategoryDto> GetAll()
+        {
+            return _service.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public IList<GetProductDto> GetProducts(int id)
+        {
+            return _service.GetProducts(id);
+        }
+
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            _service.Delete(id);
+        }
     }
 }
