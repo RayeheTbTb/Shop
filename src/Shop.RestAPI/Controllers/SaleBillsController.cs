0ca082a7413ea2e5c4146af8c71985aac5174bac
Rayehe <rayehetabatabaei1986@gmail.com>
2022-05-10 18:08:54 +0430

    Added saleBill Controlle

---
+++ src/Shop.RestAPI/Controllers/SaleBillsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Services.PurchaseBills.Contracts;
+using Shop.Services.SaleBills.Contracts;
+using System.Collections.Generic;
+
+namespace Shop.RestAPI.Controllers
+{
+    [ApiController]
+    [Route("api/salebills")]
+    public class SaleBillsController : Controller
+    {
+        private readonly SaleBillService _service;
+
+        public SaleBillsController(SaleBillService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public void Add(AddSaleBillDto dto)
+        {
+            _service.Add(dto);
+        }
+
+        [HttpPut("{id}")]
+        public void Update(int id, UpdateSaleBillDto dto)
+        {
+            _service.Update(id, dto);
+        }
+
+        [HttpGet]
+        public IList<GetSaleBillDto> GetAll()
+        {
+            return _service.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public GetSaleBillDto Get(int id)
+        {
+            return _service.Get(id);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _service.Delete(id);
+        }
+
+    }
+}
